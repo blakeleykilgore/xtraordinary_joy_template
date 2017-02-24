8e3381160bd70a7f0b2bb682b70df1344ebcb54f
Blakeley Kilgore <blakeleykilgore@gmail.com>
2017-02-23 21:35:24 -0600

    Add text widget for mission statement on homepage

--- functions.php
+++ functions.php
@@ -11,6 +11,16 @@ function blank_widgets_init() {
 		'after_title' => '</p>'
 		));
 	}
+    register_sidebar( array(
+		'name' => ('homepgae_widget2'),
+		'id' => 'homepage-widget2',
+		'description' => 'Second widget for the homepage',
+		'before_widget' => '<div class="homepage_mission_statement">',
+		'after_widget' => '</div>',
+		'before_title' => '<p class="display_none">',
+		'after_title' => '</p>'
+		));
+	}
 add_action('widgets_init', 'blank_widgets_init');
 
 /*-------------- Enable Menu --------------- */
--- index.php
+++ index.php
@@ -2,4 +2,7 @@
     <section>
         <?php dynamic_sidebar('homepage-widget1'); ?>
     </section>
+    <section>
+        <?php dynamic_sidebar('homepage-widget2'); ?>
+    </section>
 <?php get_footer(); ?>
