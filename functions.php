8bab2ad862b12b259d55887f35a7130e274abccc
Blakeley Kilgore <blakeleykilgore@gmail.com>
2017-02-23 22:54:30 -0600

    Add widgets - icon links and my story to homepage

--- functions.php
+++ functions.php
@@ -19,6 +19,24 @@ function blank_widgets_init() {
 		'before_title' => '<p class="display_none">',
 		'after_title' => '</p>'
 		));
+    register_sidebar( array(
+		'name' => ('homepgae_widget3'),
+		'id' => 'homepage-widget3',
+		'description' => 'Third widget for the homepage',
+		'before_widget' => '<div class="homepage_icon_links">',
+		'after_widget' => '</div>',
+		'before_title' => '<p class="display_none">',
+		'after_title' => '</p>'
+		));
+    register_sidebar( array(
+		'name' => ('homepgae_widget4'),
+		'id' => 'homepage-widget4',
+		'description' => 'Fourth widget for the homepage',
+		'before_widget' => '<div class="homepage_my_story">',
+		'after_widget' => '</div>',
+		'before_title' => '<p class="display_none">',
+		'after_title' => '</p>'
+		));
 	}
 add_action('widgets_init', 'blank_widgets_init');
 
--- index.php
+++ index.php
@@ -5,4 +5,10 @@
     <section>
         <?php dynamic_sidebar('homepage-widget2'); ?>
     </section>
+    <section>
+        <?php dynamic_sidebar('homepage-widget3'); ?>
+    </section>
+    <section>
+        <?php dynamic_sidebar('homepage-widget4'); ?>
+    </section>
 <?php get_footer(); ?>
