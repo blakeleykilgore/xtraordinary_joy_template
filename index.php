022fa03e0842ba5c464894d36295f8244ae7dea7
Blakeley Kilgore <blakeleykilgore@gmail.com>
2017-02-23 20:12:10 -0600

    Add semantic HTML5

--- index.php
+++ index.php
@@ -6,7 +6,24 @@
     <title>Xtraordinary Joy!</title>
 </head>
 <body>
-    <h1>Blank WordPress Template</h1>
-	<p>This is my WordPress template.</p>
+    <header>
+			<h1>Blank WordPress Template</h1>
+			<p>This is my WordPress template.</p>
+		</header>
+		<section>
+			<p>Body content goes here.</p>
+		</section>
+        <section>
+			<p>Body content goes here.</p>
+		</section>
+        <section>
+			<p>Body content goes here.</p>
+		</section>
+        <section>
+			<p>Body content goes here.</p>
+		</section>
+		<footer>
+			<p>Footer content goes here.</p>
+		</footer>
 </body>
 </html>
