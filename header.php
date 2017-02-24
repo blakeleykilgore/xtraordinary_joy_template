e75c6aad9bcaa72b9f50bf63854ec4253ab3505b
Blakeley Kilgore <blakeleykilgore@gmail.com>
2017-02-23 20:17:17 -0600

    Divide the index.php into header, index, footer

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Links to our Style.css file -->
+   <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <title>Xtraordinary Joy!</title>
+</head>
+<body>
+    <header>
+       <h1>Blank WordPress Template</h1>
+       <p>This is my WordPress template.</p>
+    </header>
--- index.php
+++ index.php
@@ -1,29 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <!-- Links to our Style.css file -->
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
-    <title>Xtraordinary Joy!</title>
-</head>
-<body>
-    <header>
-			<h1>Blank WordPress Template</h1>
-			<p>This is my WordPress template.</p>
-		</header>
-		<section>
-			<p>Body content goes here.</p>
-		</section>
-        <section>
-			<p>Body content goes here.</p>
-		</section>
-        <section>
-			<p>Body content goes here.</p>
-		</section>
-        <section>
-			<p>Body content goes here.</p>
-		</section>
-		<footer>
-			<p>Footer content goes here.</p>
-		</footer>
-</body>
-</html>
+<section>
+	<p>Body content goes here.</p>
+</section>
+<section>
+	<p>Body content goes here.</p>
+</section>
+<section>
+	<p>Body content goes here.</p>
+</section>
+<section>
+	<p>Body content goes here.</p>
+</section>
