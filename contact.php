42357e197aab6eb6bf7d95b81cb636eb68586b34
Blakeley Kilgore <blakeleykilgore@gmail.com>
2017-02-28 14:01:28 -0600

    Add page content to contact page to show form

--- contact.php
+++ contact.php
@@ -19,5 +19,14 @@
 		<section>
 	        <?php dynamic_sidebar('contact-widget3'); ?>
 	    </section>
+		<section>
+			<?php if (have_posts()) :
+				/* OUR DATA CONTEXT IS DEFINED 	*/
+				while (have_posts()) : the_post(); ?>
+					<h2><?php the_title(); ?></h2>
+					<?php the_content();
+				endwhile;
+			endif; ?>
+		</section>
 	</main>
 <?php get_footer(); ?>
