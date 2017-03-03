c74b9b57a82fcf710bed031dd8163b36e30e6dd0
Blakeley Kilgore <blakeleykilgore@gmail.com>
2017-03-03 11:31:43 -0600

    Add meta viewport tag

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <title>Xtraordinary Joy!</title>
     <?php wp_head(); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Links to our Style.css file -->
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 </head>
