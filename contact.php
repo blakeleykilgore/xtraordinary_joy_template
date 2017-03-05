740d404e5369cd208f0d1cf82201cafd79f61917
Lauren Yockey <laurenyockey@gmail.com>
2017-03-05 14:34:28 -0600

    add rows and columns to Contact page

--- contact.php
+++ contact.php
@@ -8,25 +8,34 @@
 			));
 		?>
 	</nav>
+</div>
 	<main>
 		<h2 class="display_none">Contact Page</h2>
-		<section>
-	        <?php dynamic_sidebar('contact-banner'); ?>
+		<section class="row">
+			<div class="twelve columns">
+	        	<?php dynamic_sidebar('contact-banner'); ?>
+			</div>
 	    </section>
-		<section>
-	        <?php dynamic_sidebar('contact-info'); ?>
+		<section class="row">
+			<div class="twelve columns">
+				<?php dynamic_sidebar('contact-info'); ?>
+			</div>
 	    </section>
-		<section>
-	        <?php dynamic_sidebar('contact-social-info'); ?>
+		<section class="row">
+			<div class="twelve columns">
+	        	<?php dynamic_sidebar('contact-social-info'); ?>
+			</div>
 	    </section>
-		<section>
-			<?php if (have_posts()) :
-				/* OUR DATA CONTEXT IS DEFINED 	*/
-				while (have_posts()) : the_post(); ?>
-					<h2><?php the_title(); ?></h2>
-					<?php the_content();
-				endwhile;
-			endif; ?>
+		<section class="row">
+			<div class="twelve columns">
+				<?php if (have_posts()) :
+					/* OUR DATA CONTEXT IS DEFINED 	*/
+					while (have_posts()) : the_post(); ?>
+						<h2><?php the_title(); ?></h2>
+						<?php the_content();
+					endwhile;
+				endif; ?>
+			</div>
 		</section>
 	</main>
 <?php get_footer(); ?>
