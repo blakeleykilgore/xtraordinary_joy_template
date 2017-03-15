c1fb49bcfdefd8c5d2b9360cbe1b27e620883b7f
Blakeley Kilgore <blakeleykilgore@gmail.com>
2017-03-15 13:16:37 -0500

    Adjust pages menu on each template page

--- partner.php
+++ partner.php
@@ -1,14 +1,18 @@
 <?php /* Template Name: Partner Page */ ?>
 <?php get_header(); ?>
-		<nav class="nine columns">
-			<h2 class="display_none">Navigation</h2>
-			<?php
-				wp_nav_menu(array(
-					'menu' => 'pages_menu'
-				));
-			?>
-		</nav>
-	</div>
+	<header class="three columns pages_header">
+		<img src="http://www.blakeleykilgore.com/xtraordinaryjoy/wp-content/uploads/2017/02/web_logo.png">
+		<h1 class="display_none"><?php bloginfo('name'); ?></h1>
+		<p class="display_none"><?php bloginfo('description'); ?></p>
+	</header>
+	<nav class="pages_menu">
+		<?php
+			wp_nav_menu(array(
+				'menu' => 'index_menu'
+			));
+		?>
+	</nav>
+</div>
 	<main>
 		<h2 class="display_none">Xtraordinary Joy Partner Page</h2>
 		<section class="row">
