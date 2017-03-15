12cbd71fde920505242f5450fa8f9d89482fc45c
Blakeley Kilgore <blakeleykilgore@gmail.com>
2017-03-15 13:23:21 -0500

    Add loop to Resources Page template

--- resources.php
+++ resources.php
@@ -20,5 +20,14 @@
 	        	<?php dynamic_sidebar('resources-banner'); ?>
 			</div>
 	    </section>
+		<section>
+			<?php if (have_posts()) :
+				/* OUR DATA CONTEXT IS DEFINED 	*/
+				while (have_posts()) : the_post(); ?>
+					<h2><?php the_title(); ?></h2>
+					<?php the_content();
+				endwhile;
+			endif; ?>
+		</section>
 	</main>
 <?php get_footer(); ?>
