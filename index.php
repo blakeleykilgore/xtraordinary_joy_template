f64bc9fd625f6265fb8eba61b042d8a2d9bbbfd5
Blakeley Kilgore <blakeleykilgore@gmail.com>
2017-04-03 13:31:19 -0500

    Remove negative homepage banner margins

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php get_header(); ?>
-        <header class="three columns">
-            <a href="http://www.blakeleykilgore.com/xtraordinaryjoy"><img src="http://www.blakeleykilgore.com/xtraordinaryjoy/wp-content/uploads/2017/02/web_logo.png"></a>
-            <h1 class="display_none"><?php bloginfo('name'); ?></h1>
-            <p class="display_none"><?php bloginfo('description'); ?></p>
-        </header>
-        <nav class="nine columns">
-            <h2 class="display_none">Navigation</h2>
-            <?php
-                wp_nav_menu(array(
-                    'menu' => 'index_menu'
-                ));
-            ?>
-        </nav>
-    </div>
+	<header class="three columns pages_header">
+		<a href="http://www.blakeleykilgore.com/xtraordinaryjoy"><img src="http://www.blakeleykilgore.com/xtraordinaryjoy/wp-content/uploads/2017/02/web_logo.png"></a>
+		<h1 class="display_none"><?php bloginfo('name'); ?></h1>
+		<p class="display_none"><?php bloginfo('description'); ?></p>
+	</header>
+	<nav class="pages_menu">
+		<?php
+			wp_nav_menu(array(
+				'menu' => 'index_menu'
+			));
+		?>
+	</nav>
+</div>
     <main>
         <h2 class="display_none">Xtraordinary Joy Homepage</h2>
         <section class="row">
